b90d93d81fb4ae22dec56aa5ed2c1b22702ce59f
Ahmad El-Agawy <aelagawy@gmail.com>
2018-10-25 21:23:02 +0300

    adding angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatMenuModule } from '@angular/material/menu';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TsProgrammingDemoComponent } from './ts-programming-demo/ts-programming-demo.component';
+import { MaterialDesignDemoComponent } from './material-design-demo/material-design-demo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TsProgrammingDemoComponent
+    TsProgrammingDemoComponent,
+    MaterialDesignDemoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material-design-demo/material-design-demo.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+
+@Component({
+  selector: 'app-material-design-demo',
+  templateUrl: './material-design-demo.component.html',
+  styleUrls: ['./material-design-demo.component.less']
+})
+export class MaterialDesignDemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
